245d703b67348d22f96911dc30f7def7af114bf8
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-24 22:37:27 -0300

    Subir proyecto

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container">
+        <form action="" id="form">
+            <h1>Sistema Da Lotofacil ( Logo )</h1>
+            <input type="text" name="username" id="username" placeholder="Usuario ou E-mail">
+            <input type="password" name="password" id="password" placeholder="Senha">
+            <input type="submit" value="Acessar o Sistema">
+            <div class="info">
+                <a href="#">Esqueceu sua senha?</a>
+                <label for="">
+                    <input type="checkbox" name="" id="">
+                    Lembrar-me
+                </label>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
