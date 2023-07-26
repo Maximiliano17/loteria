e27aabf48634898f60cef21eb03ae2007e2e4ebd
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-07-25 22:00:15 -0300

    Subir Home

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/login.css">
+    <title>Loteria</title>
 </head>
-<body>
+<body> 
     <div id="container">
         <form action="" id="form">
-            <h1>Sistema Da Lotofacil ( Logo )</h1>
+            <h1>( Logo )</h1>
             <input type="text" name="username" id="username" placeholder="Usuario ou E-mail">
             <input type="password" name="password" id="password" placeholder="Senha">
             <input type="submit" value="Acessar o Sistema">
             <div class="info">
-                <a href="#">Esqueceu sua senha?</a>
+                <a href="./pages/home.php">Esqueceu sua senha?</a>
                 <label for="">
                     <input type="checkbox" name="" id="">
                     Lembrar-me
---
+++ pages/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+    <title>Home/Loteria</title>
+    <!--Foonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=REM:wght@200&display=swap" rel="stylesheet">
+</head>
+<body> 
+    <div id="container">
+        <nav id="nav">
+ 
+            <h1 id="titulo">SISTEMA da LOTOFACIL</h1>
+        
+          <ul id="nav-list">
+            <li>  <a href="#">Inicio</a> </li>
+            <li>  <a href="#">Simuladores</a> </li>
+            <li>  <a href="#">Apuestas</a> </li>
+            <li>  <a href="#">Video Llamadas</a> </li>
+            <li>  <a href="#">Tendencias</a> </li>
+            <li>  <a href="#">Resultados</a> </li>
+          </ul>
+        </nav>
+        <div id="home">
+          <header id="header">
+            <p class="burger">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+            </p>
+            <p>
+            </p>
+          </header>
+        <section id="home-contenido"> 
+            Home
+        </section>
+        </div>    
+    </div> 
+</body>
+</html>
